4d51f9485c834e43570fee28542a6b0dc4eaf80c
Cephas <okodughapeter58@gmail.com>
2025-04-22 13:28:48 +0100

    Rearrange Frontend folder structure

--- FRONTEND/src/router/index.js
+++ FRONTEND/src/router/index.js
@@ -2,14 +2,11 @@ import {  createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "../views/HomeView.vue";
 import ContactPage from "../views/ContactPage.vue";
-import BeliefPage from "../views/about/BeliefPage.vue";
-import DoctrinePage from "../views/about/DoctrinePage.vue";
-import HistoryPage from "../views/about/HistoryPage.vue";
-import LeadershipPage from "../views/about/LeadershipPage.vue";
+import LeadershipPage from "../views/LeadershipPage.vue";
 import ImageGallery from "../views/media/ImageGallery.vue";
 import InstagramFeed from "../views/media/InstagramFeed.vue";
 import GivePage from "../views/GivePage.vue";
-import AboutPage from "../views/about/AboutPage.vue";
+import AboutPage from "../views/AboutPage.vue";
 
 const routes = [
   { path: "/", component: HomeView },
