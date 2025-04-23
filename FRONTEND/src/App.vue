7a64f80f55ab24256831f898a688232c32d18738
Cephas <okodughapeter58@gmail.com>
2025-04-23 13:50:09 +0100

    Make page always return to top on entry and NavBar always close on route change

--- FRONTEND/src/App.vue
+++ FRONTEND/src/App.vue
@@ -7,4 +7,14 @@
 <script setup>
 import Navigation from "./components/Navigation.vue";
 import Footer from "./components/Footer.vue";
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { useNavBarStore } from "./store/navBarStore";
+
+const route = useRoute();
+const NavBarStore = useNavBarStore();
+
+watch(route, () => {
+  NavBarStore.openMenu = false;
+});
 </script>
