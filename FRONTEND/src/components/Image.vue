c9dd12343aabd664a0f9c86b761b083b3498477c
Cephas <okodughapeter58@gmail.com>
2025-04-30 12:16:47 +0100

    Fix Lazy Load Image height Bug

--- FRONTEND/src/components/Image.vue
+++ FRONTEND/src/components/Image.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="relative">
-        <!-- Blurred Background Placeholder -->
-        <div v-if="!isLoaded" class="absolute inset-0 bg-gradient-to-r from-purple-700 to-purple-300 flex space-x-1 items-center justify-center w-full text-black">
+    <div class="relative w-full text-black">
+        <!-- Actual Lazy-Loaded Image -->
+        <img 
+            :src="imageSrc" 
+            loading="lazy"
+            :class="[isLoaded ? 'opacity-100' : 'opacity-0', `${classList} transition-opacity duration-700 object-cover`]"
+            @load="isLoaded = true"
+            :alt   
+        />
+        <!-- Placeholder Overlay -->
+        <div :class="[!isLoaded ? 'opacity-100' : 'opacity-0', 'absolute inset-0 w-full h-full object-cover bg-gradient-to-r from-purple-600 to-purple-300 flex items-center justify-center space-x-1']">
             <FontAwesomeIcon
                 :icon="['fas', 'circle']"
                 class="bounce-fast bounce-delay-1 text-xl"
@@ -15,15 +23,7 @@
                 class="bounce-fast bounce-delay-3 text-xl"
             />
         </div>
-        <!-- Actual Lazy-Loaded Image -->
-        <img 
-          v-lazy="imageSrc" 
-          :class="[ isLoaded ? 'opacity-100' : 'opacity-0', `${classList} transition-opacity duration-700 object-cover`]"
-          @load="isLoaded = true"
-          :alt   
-        />
     </div>
-
 </template>
 
 <script setup>
